723de4acc67b8abc32caa1ff09707bda4e70c75e
seainthebottle <seainthebottle@gmail.com>
2025-02-06 20:20:28 +0900

    편집 메시지 발송기능 완료

--- src/ignote-markdown-editor.js
+++ src/ignote-markdown-editor.js
@@ -2,12 +2,14 @@
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { oneDark } from "@codemirror/theme-one-dark";
 import { nord } from "cm6-theme-nord";
-import { EditorView, keymap, drawSelection, highlightActiveLine, dropCursor,
+import {
+    EditorView, keymap, drawSelection, highlightActiveLine, dropCursor,
     rectangularSelection, crosshairCursor,
-    lineNumbers, highlightActiveLineGutter } from "@codemirror/view"
-import {Compartment, StateEffect, EditorState} from "@codemirror/state"
-import {defaultKeymap, history, historyKeymap} from "@codemirror/commands"
-import {searchKeymap, highlightSelectionMatches} from "@codemirror/search"
+    lineNumbers, highlightActiveLineGutter
+} from "@codemirror/view"
+import { Compartment, StateEffect, EditorState } from "@codemirror/state"
+import { defaultKeymap, history, historyKeymap } from "@codemirror/commands"
+import { searchKeymap, highlightSelectionMatches } from "@codemirror/search"
 import MarkdownIt from "markdown-it";
 
 export default class IgnoteMarkdownEditor {
@@ -15,18 +17,27 @@ export default class IgnoteMarkdownEditor {
         this.editorContainer = editorContainer;
         //this.previewContainer = previewContainer;
         this.md = new MarkdownIt();
+        this.broadcastChannel = new BroadcastChannel("ignote_channel");
 
-        const baseFont = EditorView.theme({
-            ".cm-content": { 
-                fontSize: window.getComputedStyle(this.editorContainer).getPropertyValue('font-size'),
-                fontFamily: window.getComputedStyle(this.editorContainer).getPropertyValue('font-family'),
-                lineHeight: window.getComputedStyle(this.editorContainer).getPropertyValue('line-height')
+        const updateListener = EditorView.updateListener.of((update) => {
+            if (update.docChanged) {
+                //console.log("sent content_edited from ignote-markdown-editor")
+                // 내용 변경이 있으면 메시지를 방송한다.
+                let sendData = { command: "content_edited", data: null };
+                this.broadcastChannel.postMessage(sendData);
             }
         });
+        // setValue에서 내용변경시 updateListener가 발동되지 않도록 transaction을 pass하는 effect를 추가해야 한다.
+        this.IgnoreUpdateEffect = StateEffect.define();
+        const filterUpdateExtension = EditorState.transactionFilter.of(tr => {
+            if (tr.effects.some(e => e.is(this.IgnoreUpdateEffect))) return []; // 특정 트랜잭션을 무시
+            else return [tr];
+        });
+        
 
         const fixedHeightEditor = EditorView.theme({
-            "&.cm-editor": {height: "100%"},
-            ".cm-scroller": {overflow: "auto"}
+            "&.cm-editor": { height: "100%" },
+            ".cm-scroller": { overflow: "auto" }
         });
 
         // CodeMirror 초기화
@@ -34,7 +45,6 @@ export default class IgnoteMarkdownEditor {
             doc: initialContent,
             extensions: [
                 oneDark,
-                baseFont,
                 fixedHeightEditor,
                 EditorView.lineWrapping,
                 lineNumbers(),
@@ -50,18 +60,15 @@ export default class IgnoteMarkdownEditor {
                     ...defaultKeymap,
                     ...searchKeymap,
                     ...historyKeymap
-                ])
+                ]),
+                updateListener,
+                filterUpdateExtension 
             ]
         });
 
         this.editor = new EditorView({
             state,
             parent: this.editorContainer
-        });
-
-        // Markdown 변경 감지
-        this.editor.dispatch({
-            changes: { from: 0, to: this.editor.state.doc.length, insert: initialContent }
         });
 
         this.updatePreview();
@@ -79,10 +86,15 @@ export default class IgnoteMarkdownEditor {
         return this.editor.state.doc.toString();
     }
 
+    getOutputValue() {
+        return this.md.render(this.editor.state.doc.toString());
+    }
+
     // 값 설정하기
     setValue(content) {
         this.editor.dispatch({
-            changes: { from: 0, to: this.editor.state.doc.length, insert: content }
+            changes: { from: 0, to: this.editor.state.doc.length, insert: content },
+            effects: this.IgnoreUpdateEffect.of(null) // StateEffect 추가(업데이트 이벤트 발생을 막기 위해)
         });
         this.updatePreview();
     }
